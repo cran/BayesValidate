1d62947ddf038e12ece87b39dc750f66f2a704ca
Venelin Mitov <venelin.mitov@bsse.ethz.ch>
2018-01-29 16:55:40 +0100

    added an import foreach in DESCRIPTION. Changes in the code to allow using foreach() %dopar% construct. added argument return.all to allow returning internal objects in validate() fucntion for future analysis.

--- R/validate.R
+++ R/validate.R
@@ -1,7 +1,7 @@
 validate <- function ( generate.param, generate.param.inputs=NULL, 
 	generate.data, generate.data.inputs=NULL, analyze.data, 
-	analyze.data.inputs=NULL, n.rep=20,n.batch=NULL,params.batch=NULL, 
-	print.reps=FALSE) {
+	analyze.data.inputs=NULL, n.rep=20,n.batch=NULL,params.batch=NULL,parallel.rep=FALSE, 
+	print.reps=FALSE, return.all = FALSE) {
 
 #----------------------------------------------------------------------------
 #        Inputs
@@ -50,8 +50,17 @@ validate <- function ( generate.param, generate.param.inputs=NULL,
 #			  (e.g., params.batch=expression(alpha,beta)).  
 #			  Not used if n.batch not provided.
 # n.rep                 : number of replications to be performed, default is 20
+# parallel.rep          : logical indicating whether the replications should be executed 
+#       in parallel using the "foreach() \%dopar\% {}" construct from the R package 
+#       foreach. Defaults to FALSE. If TRUE, this parameter causes a set.seed(rep_no) to 
+#       be executed at the beginning of each replication. The user has to create and 
+#       register a parallel cluster before 
+#       calling validate() and destroy that cluster after validate() has finished.
 # print.reps            : indcator of whether or not to print the replication 
 #			  number, default is FALSE
+# return.all            : indicator whether or not to return a list with all variables,
+#       default is FALSE, meaning that only a list of p.vals, adj.min.p and where
+#       appropriate, p.batch is returned. 
 #
 #-----------------------------------------------------------------------------
 #
@@ -117,8 +126,15 @@ if(!is.null(n.batch))
 #---------------------------------------------------------------------------
 #            Validation Loop
 
-for(reps in 1:n.rep){
+`%do_op%` <- if(parallel.rep) `%dopar%` else `%do%`
 
+list.quantile.theta <- foreach(reps=1:n.rep, .packages = (.packages())) %do_op% {
+
+  if(parallel.rep) {
+    ## initiate the random generator with a unique seed for each replication.
+    set.seed(reps)
+  }
+  
 	##First generate parameters
 	##theta.true is vector of true parameters, with length n.param
 	if(is.null(generate.param.inputs)) theta.true <- generate.param() else 
@@ -162,10 +178,19 @@ for(reps in 1:n.rep){
 
 	##update matrix of posterior quantiles
 	theta.draws <- rbind(theta.true, theta.draws)
-	quantile.theta[reps, ] <- apply( theta.draws, 2, quant )
+	quantile.theta.reps <- apply( theta.draws, 2, quant )
 
-	if(print.reps==TRUE) print(reps)
+	if(print.reps==TRUE & interactive()) print(reps)
+	
+	quantile.theta.reps
 }
+
+## copy the list of quantile.theta's into the matrix (done in this separate loop 
+## to simplify returning results from the replications that can be executed in parallel.)
+for(reps in 1:n.rep) {
+  quantile.theta[reps, ] <- list.quantile.theta[[reps]]  
+}
+rm(list.quantile.theta)
 
 
 #-----------------------------------------------------------------------------
@@ -205,7 +230,7 @@ if(is.null(n.batch)){
 		plot.batch[plot.batch%in%rows.one==FALSE],
 		xlim=c(0,upper.lim),ylim=c(1,num.batches),xlab="",
 		ylab="",main=expression("Absolute "*z[theta]*" Statistics"),
-		axes=F,pch=176)
+		axes=F,pch=1)
 	##now add batch means
 	points(z.batch,c(1:num.batches),pch=20)
 	axis(1,line=.1)
@@ -216,16 +241,16 @@ abline(v=0)
 
 #----------------------------------------------------------------------------
 
-##return z statistics and p-value
-if(is.null(n.batch)){
-	return(list(p.vals = p.vals, adj.min.p=adj.min.p))} else {
-	
-	if(length(z.batch)==n.param)
-	return(list(p.batch = p.batch, adj.min.p=adj.min.p)) else 
-	return(list(p.vals = p.vals[1:n.param], p.batch = p.batch, 
-            adj.min.p=adj.min.p))
-	}
-
+if(return.all) {
+  return(mget(ls()))} else {
+  ##return z statistics and p-value
+  if(is.null(n.batch)){
+    return(list(p.vals = p.vals, adj.min.p=adj.min.p))} else {
+      
+      if(length(z.batch)==n.param)
+        return(list(p.batch = p.batch, adj.min.p=adj.min.p)) else 
+          return(list(p.vals = p.vals[1:n.param], p.batch = p.batch, 
+                      adj.min.p=adj.min.p))
+    }  
 }
-
-
+}
